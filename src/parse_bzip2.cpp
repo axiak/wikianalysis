f9042cb9c33b7a1209b907f72ea5bbc2ae27d0a6
Michael Axiak <mike@axiak.net>
2010-07-31 12:50:57 -0400

    Fixed bzip2.

--- src/parse_bzip2.cpp
+++ src/parse_bzip2.cpp
@@ -11,7 +11,7 @@ using namespace re2;
 RE2 textstart_re("<text[^>]+>");
 RE2 title_re("<title>(.+?)</title>");
 RE2 id_re("<id>(\\d+)</id>");
-RE2 link_re("\\[\\[(.+?)\\]\\]");
+RE2 link_re("(?:</text|\\[\\[(.+?)(?:\\||\\]\\]))");
 
 void inline normalize_link(string *link);
 
@@ -23,8 +23,13 @@ int main(int argc, char**argv)
     long long id = 0;
     StringPiece *matches = new StringPiece[20];
     StringPiece sline;
-    string current_title;
+    string current_title, current_link;
     bool in_text = false;
+    bool redirect = false;
+    string title;
+    int start = 0;
+    size_t tmp;
+    int num_processed = 0;
 
     while (!cin.eof()) {
         getline(cin, line);
@@ -32,7 +37,16 @@ int main(int argc, char**argv)
         sline.set(line.c_str(), line.length());
 
         if (strncmp("  <page>", line.c_str(), 8) == 0) {
+
+            if (id) {
+                adj_file << endl;
+                ++num_processed;
+                if (num_processed % 10000 == 0) {
+                    cout << "Finished with " << num_processed << endl;
+                }
+            }
             in_text = false;
+            redirect = false;
             id = 0;
             /* New page */
             getline(cin, line);
@@ -48,12 +62,18 @@ int main(int argc, char**argv)
             sline.set(line.c_str(), line.length());
             if (id_re.Match(sline, 0, RE2::UNANCHORED, matches, 2)) {
                 id = atol(matches[1].data());
-                cout << current_title << ": " << id << endl;
             }
             else {
                 continue;
             }
             id_map << id << "\t" << current_title << endl;
+            adj_file << current_title;
+
+            getline(cin, line);
+            if (strncmp(line.c_str(), "    <redirect", 13) == 0) {
+                /* This is a redirect */
+                redirect = true;
+            }
             continue;
         }
 
@@ -66,6 +86,7 @@ int main(int argc, char**argv)
             /* This is the beginning of a text. */
             line = line.substr(matches[0].length() + 
                                ((int)matches[0].data() - (int)line.c_str()));
+            sline.set(line.c_str(), line.length());
             if (id)
                 in_text = true;
         }
@@ -73,9 +94,28 @@ int main(int argc, char**argv)
         if (!in_text)
             continue;
 
+        start = 0;
         /* Now we look for all links. */
-        
-        
+        while (link_re.Match(sline, start, RE2::UNANCHORED, matches, 2)) {
+            if (strncmp(matches[0].data(), "</text", 6) == 0) {
+                in_text = false;
+                break;
+            }
+            start = matches[0].length() + ((int)matches[0].data() - (int)sline.data());
+            current_link = matches[1].as_string();
+            tmp = current_link.find_first_of(':');
+            if (tmp != -1 && tmp < 8) {
+                continue;
+            }
+            normalize_link(&current_link);
+            if (redirect) {
+                adj_file << "\tR:" << current_link;
+            }
+            else {
+                adj_file << "\t" << current_link;
+            }                
+            //cout << current_link << endl;
+        }
     }
 
     return 0;
@@ -86,10 +126,8 @@ void inline normalize_link(string *link)
 {
     for (int i=0; i < link->length(); i++) {
         if ((*link)[i] == ' ') {
-            (*link)[i] == '_';
+            (*link)[i] = '_';
         } 
-        else if (i > 0) {
-            (*link)[i] = tolower((*link)[i]);
-        }
     }
+    (*link)[0] = toupper((*link)[0]);
 }
